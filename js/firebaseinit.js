b92891301112d8a0e660fd847162b20a530bd2cf
Muhammad Azhar Iqbal <muhammad1azhar2@gmail.com>
2021-01-01 12:47:32 +0500

    1/1/2021

---
+++ js/firebaseinit.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBOCwz-dzu09Ajjn9P_uAncnUHaqv3j5m8",
+    authDomain: "chat-webapplication.firebaseapp.com",
+    projectId: "chat-webapplication",
+    storageBucket: "chat-webapplication.appspot.com",
+    messagingSenderId: "780540670156",
+    appId: "1:780540670156:web:e628fa57ff387762f75269"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
