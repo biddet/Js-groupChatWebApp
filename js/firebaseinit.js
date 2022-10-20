985ab345eed50c26d2db927fce81b26454ae70c6
biddet <96024863+biddet@users.noreply.github.com>
2022-10-20 10:02:10 -0400

    Update firebaseinit.js

--- js/firebaseinit.js
+++ js/firebaseinit.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDbTrmmdCZeAbIc9VJttSGEV6cp6QLd5Oc",
-  authDomain: "groupchatwebapp.firebaseapp.com",
-  projectId: "groupchatwebapp",
-  storageBucket: "groupchatwebapp.appspot.com",
-  messagingSenderId: "675122513762",
-  appId: "1:675122513762:web:585bb7e3cda312e57d0e3b"
+   apiKey: "AIzaSyC9eVMZE0sNUjPviZnrn2Y1lUIadTWhBeY",
+  authDomain: "friend-lychat.firebaseapp.com",
+  databaseURL: "https://friend-lychat-default-rtdb.firebaseio.com",
+  projectId: "friend-lychat",
+  storageBucket: "friend-lychat.appspot.com",
+  messagingSenderId: "634719125304",
+  appId: "1:634719125304:web:567bdccbdaa7b5f99e8bcd"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
