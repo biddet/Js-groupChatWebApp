f29119aab6eac0a7bb31481b42bf5e58480776d5
biddet <96024863+biddet@users.noreply.github.com>
2022-10-21 10:30:49 -0400

    Update chat-home.js

--- js/chat-home.js
+++ js/chat-home.js
@@ -117,30 +117,6 @@ firebase.auth().onAuthStateChanged((user)=>{
     } else {window.location="signin.html"}
 })
 
-window.addEventListener("DOMContentLoaded", function(event){
-  // Once DOM is loaded add onclick function
-  // Select all buttons with class "btn-delete"
-  document.querySelectorAll('button.btn-delete').forEach(el => {
-    // Walk each selected button and set onclick function
-    // remember to declare event function within a wrapper if you're gonna pass a var
-    el.onclick = () => deleteItem(el)
-    /* "() => Code" is called arrow function
-    * it's the same as a regular anonymous function
-      function(){Code}
-    */
-    // if you want to just execute the function
-    // el.onclick = deleteItem
-  })
-});
-// Declare your function outer the DOMContentLoaded event listener
-function deleteItem(e){
- parent = ref.parent()
-parent.on('child_removed', function (snapshot) {
-    // removed!
-})
-ref.remove()
-}
-
 var logout=(event)=>{
  event.preventDefault()
  firebase.auth().signOut()
